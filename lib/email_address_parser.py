158b435cd751132670d63e0fd11dfe1cd52e4f33
Al Cerdan Lico <alcerdan@yahoo.com>
2024-12-06 11:13:47 -0800

    complete python email parser lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+    
+    def parse(self):
+        potential_emails = re.split(r'[,\s]+', self.email_addresses.strip())
+        email_pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.\w+$')
+        valid_emails = [email for email in potential_emails if email_pattern.match(email)]
+        return sorted(valid_emails)
+
